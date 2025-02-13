66a4a1b3395742c8fde26b1a050cff987150be3b
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2025-02-13 19:05:30 +0900

    ✨ update post repository

--- src/repositories/posts.repository.js
+++ src/repositories/posts.repository.js
@@ -1,1 +1,22 @@
-export class PostsRepository {}
+import { prisma } from '../utils/prisma/index.js';
+
+export class PostsRepository {
+  async findAllPosts() {
+    const posts = await prisma.posts.findMany();
+
+    return posts;
+  }
+
+  async createPost(nickname, password, title, content) {
+    const createdPosts = await prisma.posts.create({
+      data: {
+        nickname,
+        password,
+        title,
+        content,
+      },
+    });
+
+    return createdPosts;
+  }
+}
