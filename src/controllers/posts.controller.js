49dd0a341e7e502bd72b7af6acf2bc4f7a3804ab
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2025-02-13 19:53:39 +0900

    🩹 fix postsService 인스턴스 위치 변경

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -1,12 +1,14 @@
 import { PostsService } from '../services/posts.service.js';
 
+const postsService = new PostsService();
 export class PostsController {
   postsService = new PostsService();
 
   // 게시글 조회 API
   async getPost(req, res, next) {
     try {
-      const posts = await this.postsService.findAllPosts();
+      const posts = await postsService.findAllPosts();
+
       return res.status(200).json(posts);
     } catch (error) {
       next(error);
@@ -18,7 +20,7 @@ export class PostsController {
     try {
       const { nickname, password, title, content } = req.body;
 
-      const createdPost = await this.postsService.createPost(nickname, password, title, content);
+      const createdPost = await postsService.createPost(nickname, password, title, content);
       return res.status(201).json(createdPost);
     } catch (error) {
       next(error);
