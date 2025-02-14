8a26ee1f26f9c559ed63d9a38f275b9a055b1c82
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2025-02-14 15:52:30 +0900

    🩹 fix 생성자에서 Service 필드 생성

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -1,69 +1,69 @@
-import { PostsService } from '../services/posts.service.js';
-
-const postsService = new PostsService();
 export class PostsController {
-  postsService = new PostsService();
+  constructor(postsService) {
+    this.postsService = postsService;
+  }
 
   // 게시글 조회 API
-  async getPost(req, res, next) {
+  getPost = async (req, res, next) => {
     try {
-      const posts = await postsService.findAllPosts();
-
+      const posts = await this.postsService.findAllPosts();
       return res.status(200).json(posts);
     } catch (error) {
       next(error);
     }
-  }
+  };
 
   // 게시글 생성 API
-  async createPost(req, res, next) {
+  createPost = async (req, res, next) => {
     try {
       const { nickname, password, title, content } = req.body;
 
-      const createdPost = await postsService.createPost(nickname, password, title, content);
+      if (!nickname || !password || !title || !content) throw new Error('InvalidParamsError');
+
+      const createdPost = await this.postsService.createPost(nickname, password, title, content);
       return res.status(201).json(createdPost);
     } catch (error) {
       next(error);
     }
-  }
+  };
 
   // 게시글 상세 조회 API
-  async getPostById(req, res, next) {
+  getPostById = async (req, res, next) => {
     try {
       const { postId } = req.params;
 
-      const post = await postsService.findPostById(postId);
+      const post = await this.postsService.findPostById(postId);
 
       return res.status(200).json(post);
     } catch (error) {
       next(error);
     }
-  }
+  };
 
-  async updatePost(req, res, next) {
+  updatePost = async (req, res, next) => {
     try {
       const { postId } = req.params;
 
       const { password, title, content } = req.body;
 
-      const updatedPost = await postsService.updatePost(postId, password, title, content);
+      const updatedPost = await this.postsService.updatePost(postId, password, title, content);
 
       return res.status(200).json(updatedPost);
     } catch (error) {
       next(error);
     }
-  }
+  };
 
-  async deletePost(req, res, next) {
+  deletePost = async (req, res, next) => {
     try {
       const { postId } = req.param;
       const { password } = req.body;
 
-      const deletedPost = await postsService.deletePost(postId, password);
+      const deletedPost = await this.postsService.deletePost(postId, password);
 
       return res.status(200).json(deletedPost);
     } catch (error) {
       next(error);
     }
-  }
+  };
 }
